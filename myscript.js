b3687dd6b15add20ecdb11224f535d249dce47a4
Yuta Matsumura <dev.tsubaki@gmail.com>
2013-05-26 19:31:05 +0900

    complete #5

--- myscript.js
+++ myscript.js
@@ -3,6 +3,11 @@ var mainCtrl = function($scope) {
 		{ "name":"matsumura", "score":52.22 }
 		, { "name":"tanaka", "score":76.05 }
 		, { "name":"yamada", "score":48.30 }
+		, { "name":"satou", "score":3.14 }
+		, { "name":"yamamoto", "score":12.43 }
+		, { "name":"takahashi", "score":30.3 }
+		, { "name":"itou", "score":39.45 }
+		, { "name":"oda", "score":18.4 }
 	];
 	$scope.today = new Date();
 }
