48e8b42e6b63f0fa82e421d93410ce9c1e0013da
tsubakimoto <tkmbmbr@gmail.com>
2014-03-02 21:47:35 +0900

    #03

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var mainCtrl = function($scope) {
+    $scope.users = [
+        {"name":"yuta", "score":52.22},
+        {"name":"saori", "score":38.22},
+        {"name":"tsubaki", "score":67},
+    ];
+}
