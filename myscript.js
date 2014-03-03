1be2df64dd427fad10b2eeca7893bdcbea7cce99
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-03 22:12:19 +0900

    #05

--- myscript.js
+++ myscript.js
@@ -1,9 +1,13 @@
 var mainCtrl = function($scope) {
     $scope.users = [
-        {"name":"yuta", "score":52.22},
-        {"name":"saori", "score":38.22},
-        {"name":"tsubaki", "score":67},
+        {"name":"yuta1", "score":52.22},
+        {"name":"saori1", "score":38.22},
+        {"name":"tsubaki1", "score":67},
+        {"name":"yuta2", "score":5.2},
+        {"name":"saori2", "score":82.4},
+        {"name":"tsubaki2", "score":43.12},
+        {"name":"yuta3", "score":30.3},
+        {"name":"saori3", "score":8.9},
+        {"name":"tsubaki3", "score":72.1}
     ];
-    
-    $scope.today = new Date();
 }
