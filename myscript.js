a5c84b423a2b467ec15585416be5c3db4c6bc4a2
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-03 23:14:25 +0900

    #08,#09

--- myscript.js
+++ myscript.js
@@ -1,21 +1,5 @@
 var mainCtrl = function($scope) {
-    $scope.users = [
-        {"name":"yuta1", "score":52.22},
-        {"name":"saori1", "score":38.22},
-        {"name":"tsubaki1", "score":67},
-        {"name":"yuta2", "score":5.2},
-        {"name":"saori2", "score":82.4},
-        {"name":"tsubaki2", "score":43.12},
-        {"name":"yuta3", "score":30.3},
-        {"name":"saori3", "score":8.9},
-        {"name":"tsubaki3", "score":72.1}
-    ];
-}
-
-// ng-controllerを入れ子にすると親要素のデータにアクセスできる
-var userItemCtrl = function($scope) {
-    $scope.increment = function() {
-        // $scope.user は親要素のデータ
-        $scope.user.score++;
+    $scope.addUser = function() {
+        // 省略
     }
 }
