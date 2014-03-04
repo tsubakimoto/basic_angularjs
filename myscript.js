7ff4fe7d52153d57e5741cdac3491dff50fe726d
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-04 22:55:40 +0900

    #12-データと初期値をJSで定義

--- myscript.js
+++ myscript.js
@@ -2,4 +2,12 @@ var mainCtrl = function($scope) {
     $scope.addUser = function() {
         // 省略
     }
+    
+    $scope.colors = [
+        'red',
+        'blue',
+        'pink'
+    ];
+    
+    $scope.initColor = $scope.colors[2];
 }
